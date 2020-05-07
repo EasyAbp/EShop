0fba7074da0c6842704ae78e898088abf2c403cd
gdlcf88 <47396430@qq.com>
2020-05-08 03:46:34 +0800

    Updated to ABP v2.7.0

--- samples/EasyMall/aspnet-core/src/EasyMall.Web/wwwroot/libs/abp/jquery/abp.jquery.js
+++ samples/EasyMall/aspnet-core/src/EasyMall.Web/wwwroot/libs/abp/jquery/abp.jquery.js
@@ -255,7 +255,7 @@ var abp = abp || {};
     });
 
     abp.event.on('abp.configurationInitialized', function () {
-        var l = abp.localization.getResource('EasyAbpEShopAbpUi');
+        var l = abp.localization.getResource('AbpUi');
 
         abp.ajax.defaultError.message = l('DefaultErrorMessage');
         abp.ajax.defaultError.details = l('DefaultErrorMessageDetail');
