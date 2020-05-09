428e5618858329bfc57ef3a736a36128b348191b
gdlcf88 <47396430@qq.com>
2020-05-09 14:45:07 +0800

    Fix push_packages.ps1

--- nupkg/push_packages.ps1
+++ nupkg/push_packages.ps1
@@ -1,12 +1,19 @@
 . ".\common.ps1"
 
-# Get the version
-[xml]$commonPropsXml = Get-Content (Join-Path $rootFolder "common.props")
-$version = $commonPropsXml.Project.PropertyGroup.Version
+$keywords = ".Application.Contracts", ".Domain.Shared", ".HttpApi.Client", ".Application", ".Domain", ".EntityFrameworkCore", ".HttpApi", ".MongoDB", ".Web"
 
 # Publish all packages
 foreach($project in $projects) {
     $projectName = $project.Substring($project.LastIndexOf("/") + 1)
+	$moduleName = $projectName
+	foreach($keyword in $keywords) {
+		$moduleName = $moduleName.Replace($keyword, "")
+	}
+	$moduleFolder = Join-Path (Join-Path $rootFolder "modules") $moduleName
+	Write-Host $moduleFolder
+	# Get the version
+	[xml]$commonPropsXml = Get-Content (Join-Path $moduleFolder "common.props")
+	$version = $commonPropsXml.Project.PropertyGroup.Version
     & dotnet nuget push ($projectName + "." + $version + ".nupkg") -s https://api.nuget.org/v3/index.json
 }
 
