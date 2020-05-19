30b4185dc34c19a9ee451e8e4d5ebda868e59bed
gdlcf88 <47396430@qq.com>
2020-05-19 18:02:52 +0800

    Removed WeChatPay module

--- modules/EasyAbp.EShop.Payments.WeChatPay/src/EasyAbp.EShop.Payments.WeChatPay.Domain.Shared/EasyAbp/EShop/Payments/WeChatPay/WeChatPayErrorCodes.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EasyAbp.EShop.Payments.WeChatPay
-{
-    public static class WeChatPayErrorCodes
-    {
-        //Add your business exception error codes here...
-    }
-}
--- modules/EasyAbp.EShop.Payments.WeChatPay/src/EasyAbp.EShop.Payments.WeChatPay.Web/Pages/EShop/Payments/WeChatPay/Index.cshtml.cs
+++
@@ -1,9 +0,0 @@
-namespace EasyAbp.EShop.Payments.WeChatPay.Web.Pages.EShop.Payments.WeChatPay
-{
-    public class IndexModel : WeChatPayPageModel
-    {
-        public void OnGet()
-        {
-        }
-    }
-}
