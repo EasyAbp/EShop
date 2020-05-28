1eaeab25b65fabdac6812071677fa49eb46adb83
gdlcf88 <47396430@qq.com>
2020-05-28 20:31:56 +0800

    Added angular project

---
+++ samples/EasyMall/angular/src/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+  production: false,
+  application: {
+    name: 'EasyMall',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44340',
+    clientId: 'EasyMall_App',
+    dummyClientSecret: '1q2w3e*',
+    scope: 'EasyMall',
+    showDebugInformation: true,
+    oidc: false,
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44340',
+    },
+  },
+  localization: {
+    defaultResourceName: 'EasyMall',
+  },
+};
