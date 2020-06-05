eb048adddc4f4f169c8819a240fec3c05ee3d5b8
gdlcf88 <gdlcf88@outlook.com>
2020-06-05 18:05:20 +0800

    Changed permission names in the payments module

--- modules/EasyAbp.EShop.Payments/src/EasyAbp.EShop.Payments.Application.Contracts/EasyAbp/EShop/Payments/Authorization/PaymentsPermissions.cs
+++ modules/EasyAbp.EShop.Payments/src/EasyAbp.EShop.Payments.Application.Contracts/EasyAbp/EShop/Payments/Authorization/PaymentsPermissions.cs
@@ -13,7 +13,7 @@ namespace EasyAbp.EShop.Payments.Authorization
 
         public class Payments
         {
-            public const string Default = GroupName + ".Payments";
+            public const string Default = GroupName + ".Payment";
             public const string Manage = Default + ".Manage";
             public const string CrossStore = Default + ".CrossStore";
             public const string Create = Default + ".Create";
@@ -22,7 +22,7 @@ namespace EasyAbp.EShop.Payments.Authorization
 
         public class Refunds
         {
-            public const string Default = GroupName + ".Refunds";
+            public const string Default = GroupName + ".Refund";
             public const string Manage = Default + ".Manage";
             public const string CrossStore = Default + ".CrossStore";
             public const string Create = Default + ".Create";
