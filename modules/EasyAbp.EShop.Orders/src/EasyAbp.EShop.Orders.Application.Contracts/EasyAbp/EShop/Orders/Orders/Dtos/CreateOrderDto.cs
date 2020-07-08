80253d4b4d09924e5ceb70286c47c5e2fe9e762b
gdlcf88 <gdlcf88@outlook.com>
2020-07-08 17:26:55 +0800

    Make CreateOrderDto implement IHasExtraProperties

--- modules/EasyAbp.EShop.Orders/src/EasyAbp.EShop.Orders.Application.Contracts/EasyAbp/EShop/Orders/Orders/Dtos/CreateOrderDto.cs
+++ modules/EasyAbp.EShop.Orders/src/EasyAbp.EShop.Orders.Application.Contracts/EasyAbp/EShop/Orders/Orders/Dtos/CreateOrderDto.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using Volo.Abp.Data;
 
 namespace EasyAbp.EShop.Orders.Orders.Dtos
 {
-    public class CreateOrderDto : IValidatableObject
+    public class CreateOrderDto : IHasExtraProperties, IValidatableObject
     {
         [DisplayName("OrderStoreId")]
         public Guid StoreId { get; set; }
