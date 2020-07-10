ea2b63e57642640b99a60dbb6a489e3a635c428c
gdlcf88 <gdlcf88@outlook.com>
2020-07-10 21:06:23 +0800

    Fix CategoryDto

--- modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Application.Contracts/EasyAbp/EShop/Products/Categories/Dtos/CategoryDto.cs
+++ modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Application.Contracts/EasyAbp/EShop/Products/Categories/Dtos/CategoryDto.cs
@@ -1,13 +1,22 @@
 using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
 using Volo.Abp.Application.Dtos;
 
 namespace EasyAbp.EShop.Products.Categories.Dtos
 {
     public class CategoryDto : FullAuditedEntityDto<Guid>
     {
-        public Guid? ParentCategoryId { get; set; }
-
         public string DisplayName { get; set; }
+        
+        [NotNull]
+        public string Code { get; set; }
+        
+        public int Level { get; set; }
+        
+        public Guid? ParentId { get; set; }
+        
+        public ICollection<CategoryDto> Children { get; set; }
 
         public string Description { get; set; }
 
