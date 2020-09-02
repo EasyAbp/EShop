4a98ee4d43f9bb3a45ff9fcbd69d5da4d51a73c2
gdlcf88 <gdlcf88@outlook.com>
2020-09-02 17:56:07 +0800

    Add migration in EShopSample

---
+++ samples/EShopSample/aspnet-core/src/EShopSample.EntityFrameworkCore.DbMigrations/Migrations/20200902095440_AddedExtraPropertiesToProductSku.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShopSample.Migrations
+{
+    public partial class AddedExtraPropertiesToProductSku : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExtraProperties",
+                table: "EasyAbpEShopProductsProductSkus",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExtraProperties",
+                table: "EasyAbpEShopProductsProductSkus");
+        }
+    }
+}
