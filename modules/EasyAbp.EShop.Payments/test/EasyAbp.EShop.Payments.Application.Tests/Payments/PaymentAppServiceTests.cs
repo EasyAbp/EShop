b9b58505540aa3bb98b086b9767b1c7738418e8f
GameBelial <243387971@qq.com>
2020-09-08 19:10:22 +0800

    Complete the basic unit test of the payments module.

--- modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.Application.Tests/Payments/PaymentAppServiceTests.cs
+++ modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.Application.Tests/Payments/PaymentAppServiceTests.cs
@@ -1,5 +1,11 @@
-using Shouldly;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using EasyAbp.EShop.Orders.Orders;
+using EasyAbp.EShop.Orders.Orders.Dtos;
+using EasyAbp.EShop.Payments.Payments.Dtos;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 using Xunit;
 
 namespace EasyAbp.EShop.Payments.Payments
@@ -8,19 +14,46 @@ namespace EasyAbp.EShop.Payments.Payments
     {
         private readonly IPaymentAppService _paymentAppService;
 
+        protected override void AfterAddApplication(IServiceCollection services)
+        {
+            MockOrderService(services);
+        }
+
+        private void MockOrderService(IServiceCollection services)
+        {
+            var orderService = Substitute.For<IOrderAppService>();
+            orderService.GetAsync(PaymentsTestData.Order1).Returns(Task.FromResult(new OrderDto
+            {
+                Id = PaymentsTestData.Order1,
+                Currency = "$",
+                TotalDiscount = 10,
+                TotalPrice = 100,
+                StoreId = PaymentsTestData.Store1
+            }));
+            
+            services.AddTransient(_ => orderService);
+        }
+
         public PaymentAppServiceTests()
         {
             _paymentAppService = GetRequiredService<IPaymentAppService>();
         }
 
         [Fact]
-        public async Task Test1()
+        public async Task Should_Success_Create_Payment()
         {
             // Arrange
+            var request = new CreatePaymentDto
+            {
+                OrderIds = new List<Guid>
+                {
+                    PaymentsTestData.Order1
+                },
+                PaymentMethod = "Alipay"
+            };
 
-            // Act
-
-            // Assert
+            // Act & Assert
+            await _paymentAppService.CreateAsync(request);
         }
     }
 }
--- modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.Application.Tests/Refunds/RefundAppServiceTests.cs
+++ modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.Application.Tests/Refunds/RefundAppServiceTests.cs
@@ -1,5 +1,6 @@
-using Shouldly;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using EasyAbp.EShop.Payments.Refunds.Dtos;
 using Xunit;
 
 namespace EasyAbp.EShop.Payments.Refunds
@@ -13,14 +14,30 @@ namespace EasyAbp.EShop.Payments.Refunds
             _refundAppService = GetRequiredService<IRefundAppService>();
         }
 
+        // TODO: There may be BUG.
         [Fact]
-        public async Task Test1()
+        public async Task Should_Refund_Success()
         {
             // Arrange
+            var request = new CreateEShopRefundInput
+            {
+                DisplayReason = "Reason",
+                CustomerRemark = "Customer Remark",
+                PaymentId = PaymentsTestData.Payment1,
+                StaffRemark = "StaffRemark",
+                RefundItems = new List<CreateEShopRefundItemInput>
+                {
+                    new CreateEShopRefundItemInput
+                    {
+                        CustomerRemark = "CustomerRemark",
+                        OrderId = PaymentsTestData.Order1,
+                        StaffRemark = "StaffRemark"
+                    }
+                }
+            };
 
-            // Act
-
-            // Assert
+            // Act & Assert
+            await _refundAppService.CreateAsync(request);
         }
     }
 }
