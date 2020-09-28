d3c944d98b85bb00fddf1d77d9b06d00bbd0b617
gdlcf88 <gdlcf88@outlook.com>
2020-09-28 16:46:14 +0800

    Change StoreOwner to audited aggregate root

---
+++ samples/EShopSample/aspnet-core/src/EShopSample.EntityFrameworkCore.DbMigrations/Migrations/20200928084506_ChangedStoreOwnerToAuditedAggregateRoot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EShopSample.Migrations
+{
+    public partial class ChangedStoreOwnerToAuditedAggregateRoot : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeleterId",
+                table: "EasyAbpEShopStoresStoreOwners");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "EasyAbpEShopStoresStoreOwners");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "EasyAbpEShopStoresStoreOwners");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DeleterId",
+                table: "EasyAbpEShopStoresStoreOwners",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "EasyAbpEShopStoresStoreOwners",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "EasyAbpEShopStoresStoreOwners",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
