de1a3f57428161b8642d61f9ecf7a4edfbcf7cea
gdlcf88 <gdlcf88@outlook.com>
2020-12-01 14:49:37 +0800

    Add ctor to OrderExtraFeeInfoModel class

--- modules/EasyAbp.EShop.Orders/src/EasyAbp.EShop.Orders.Application/EasyAbp/EShop/Orders/Orders/OrderExtraFeeInfoModel.cs
+++ modules/EasyAbp.EShop.Orders/src/EasyAbp.EShop.Orders.Application/EasyAbp/EShop/Orders/Orders/OrderExtraFeeInfoModel.cs
@@ -11,5 +11,15 @@ namespace EasyAbp.EShop.Orders.Orders
         public string Key { get; set; }
         
         public decimal Fee { get; set; }
+
+        public OrderExtraFeeInfoModel(
+            [NotNull] string name,
+            [CanBeNull] string key,
+            decimal fee)
+        {
+            Name = name;
+            Key = key ?? string.Empty;
+            Fee = fee;
+        }
     }
 }
