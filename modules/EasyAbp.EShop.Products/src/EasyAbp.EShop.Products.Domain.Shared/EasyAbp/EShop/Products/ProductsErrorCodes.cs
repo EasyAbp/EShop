580481943cbca2e7f72220f5bf038296f99f2049
yinchang0626 <yinchang0626@gmail.com>
2022-03-11 13:27:08 +0800

    remove ErrorCodes namespace

--- modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Domain.Shared/EasyAbp/EShop/Products/ProductsErrorCodes.cs
+++ modules/EasyAbp.EShop.Products/src/EasyAbp.EShop.Products.Domain.Shared/EasyAbp/EShop/Products/ProductsErrorCodes.cs
@@ -2,13 +2,13 @@
 {
     public static class ProductsErrorCodes
     {
-        public const string DuplicateCategoryUniqueName = "EasyAbp.EShop.Products.ErrorCodes:DuplicateCategoryUniqueName";
-        public const string DuplicatedProductUniqueName = "EasyAbp.EShop.Products.ErrorCodes:DuplicatedProductUniqueName";
-        public const string InventoryChangeFailed = "EasyAbp.EShop.Products.ErrorCodes:InventoryChangeFailed";
-        public const string NonexistentProductGroup = "EasyAbp.EShop.Products.ErrorCodes:NonexistentProductGroup";
-        public const string ProductDetailHasBeenUsed = "EasyAbp.EShop.Products.ErrorCodes:ProductDetailHasBeenUsed";
-        public const string ProductSkuCodeDuplicated = "EasyAbp.EShop.Products.ErrorCodes:ProductSkuCodeDuplicated";
-        public const string ProductSkuDuplicated = "EasyAbp.EShop.Products.ErrorCodes:ProductSkuDuplicated";
+        public const string DuplicateCategoryUniqueName = "EasyAbp.EShop.Products:DuplicateCategoryUniqueName";
+        public const string DuplicatedProductUniqueName = "EasyAbp.EShop.Products:DuplicatedProductUniqueName";
+        public const string InventoryChangeFailed = "EasyAbp.EShop.Products:InventoryChangeFailed";
+        public const string NonexistentProductGroup = "EasyAbp.EShop.Products:NonexistentProductGroup";
+        public const string ProductDetailHasBeenUsed = "EasyAbp.EShop.Products:ProductDetailHasBeenUsed";
+        public const string ProductSkuCodeDuplicated = "EasyAbp.EShop.Products:ProductSkuCodeDuplicated";
+        public const string ProductSkuDuplicated = "EasyAbp.EShop.Products:ProductSkuDuplicated";
         //Add your business exception error codes here...
     }
 }
