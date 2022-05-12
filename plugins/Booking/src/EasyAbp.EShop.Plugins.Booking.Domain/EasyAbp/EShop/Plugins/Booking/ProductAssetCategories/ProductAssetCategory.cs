713b00da280e4e95332ad2d1294195e51bd5db4a
gdlcf88 <gdlcf88@outlook.com>
2022-05-12 19:50:21 +0800

    Add properties `FromTime` and `ToTime` to ProductAsset and ProductAssetCategory

--- plugins/Booking/src/EasyAbp.EShop.Plugins.Booking.Domain/EasyAbp/EShop/Plugins/Booking/ProductAssetCategories/ProductAssetCategory.cs
+++ plugins/Booking/src/EasyAbp.EShop.Plugins.Booking.Domain/EasyAbp/EShop/Plugins/Booking/ProductAssetCategories/ProductAssetCategory.cs
@@ -7,6 +7,7 @@ namespace EasyAbp.EShop.Plugins.Booking.ProductAssetCategories;
 /// <summary>
 /// Mapping of ProductSku to AssetCategory.
 /// Set which SKU can use to book a specified asset category.
+/// The matched <see cref="ProductAssetCategory"/> with the larger <see cref="FromTime"/> takes precedence.
 /// </summary>
 public class ProductAssetCategory : AuditedAggregateRoot<Guid>, IMultiTenant
 {
@@ -17,6 +18,17 @@ public class ProductAssetCategory : AuditedAggregateRoot<Guid>, IMultiTenant
     public virtual Guid ProductSkuId { get; protected set; }
     
     public virtual Guid AssetCategoryId { get; protected set; }
+    
+    /// <summary>
+    /// When will this mapping start taking effect.
+    /// </summary>
+    public virtual DateTime FromTime { get; protected set; }
+    
+    /// <summary>
+    /// When will this mapping stop taking effect.
+    /// Setting to <c>null</c> means until forever.
+    /// </summary>
+    public virtual DateTime? ToTime { get; protected set; }
     
     /// <summary>
     /// Fall back to the price of ProductSku if <c>null</c>.
--- plugins/Booking/src/EasyAbp.EShop.Plugins.Booking.Domain/EasyAbp/EShop/Plugins/Booking/ProductAssets/ProductAsset.cs
+++ plugins/Booking/src/EasyAbp.EShop.Plugins.Booking.Domain/EasyAbp/EShop/Plugins/Booking/ProductAssets/ProductAsset.cs
@@ -7,6 +7,7 @@ namespace EasyAbp.EShop.Plugins.Booking.ProductAssets;
 /// <summary>
 /// Mapping of ProductSku to Asset.
 /// Set which SKU can use to book a specified asset.
+/// The matched <see cref="ProductAsset"/> with the larger <see cref="FromTime"/> takes precedence.
 /// </summary>
 public class ProductAsset : AuditedAggregateRoot<Guid>, IMultiTenant
 {
@@ -17,6 +18,17 @@ public class ProductAsset : AuditedAggregateRoot<Guid>, IMultiTenant
     public virtual Guid ProductSkuId { get; protected set; }
     
     public virtual Guid AssetId { get; protected set; }
+    
+    /// <summary>
+    /// When will this mapping start taking effect.
+    /// </summary>
+    public virtual DateTime FromTime { get; protected set; }
+    
+    /// <summary>
+    /// When will this mapping stop taking effect.
+    /// Setting to <c>null</c> means until forever.
+    /// </summary>
+    public virtual DateTime? ToTime { get; protected set; }
     
     /// <summary>
     /// Fall back to the price of ProductSku if <c>null</c>.
