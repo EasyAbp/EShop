f738f80a65fc6cfc24f81d3acbba1c623b5501f7
gdlcf88 <gdlcf88@outlook.com>
2022-07-19 01:51:03 +0800

    Update pages to support "check-create-order"

--- plugins/Baskets/src/EasyAbp.EShop.Plugins.Baskets.Domain.Shared/EasyAbp/EShop/Plugins/Baskets/BasketsErrorCodes.cs
+++ plugins/Baskets/src/EasyAbp.EShop.Plugins.Baskets.Domain.Shared/EasyAbp/EShop/Plugins/Baskets/BasketsErrorCodes.cs
@@ -2,6 +2,7 @@
 {
     public static class BasketsErrorCodes
     {
+        public const string CheckCreateOrderFailed = "EasyAbp.EShop.Plugins.Baskets:CheckCreateOrderFailed";
         public const string ProductSkuNotFound = "EasyAbp.EShop.Plugins.Baskets:ProductSkuNotFound";
     }
 }
--- plugins/Baskets/src/EasyAbp.EShop.Plugins.Baskets.Web/Pages/EShop/Plugins/Baskets/BasketItems/BasketItem/EditModal.cshtml.cs
+++ plugins/Baskets/src/EasyAbp.EShop.Plugins.Baskets.Web/Pages/EShop/Plugins/Baskets/BasketItems/BasketItem/EditModal.cshtml.cs
@@ -1,12 +1,16 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using EasyAbp.EShop.Orders.Orders;
+using EasyAbp.EShop.Orders.Orders.Dtos;
 using Microsoft.AspNetCore.Mvc;
 using EasyAbp.EShop.Plugins.Baskets.BasketItems;
 using EasyAbp.EShop.Plugins.Baskets.BasketItems.Dtos;
 using EasyAbp.EShop.Plugins.Baskets.Permissions;
 using EasyAbp.EShop.Plugins.Baskets.Web.Pages.EShop.Plugins.Baskets.BasketItems.BasketItem.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Volo.Abp;
 using Volo.Abp.Settings;
 
 namespace EasyAbp.EShop.Plugins.Baskets.Web.Pages.EShop.Plugins.Baskets.BasketItems.BasketItem
@@ -27,11 +31,15 @@ namespace EasyAbp.EShop.Plugins.Baskets.Web.Pages.EShop.Plugins.Baskets.BasketIt
         [BindProperty]
         public EditBasketItemViewModel ViewModel { get; set; }
 
-        private readonly IBasketItemAppService _service;
+        private readonly IOrderAppService _orderAppService;
+        private readonly IBasketItemAppService _basketItemAppService;
 
-        public EditModalModel(IBasketItemAppService service)
+        public EditModalModel(
+            IOrderAppService orderAppService,
+            IBasketItemAppService basketItemAppService)
         {
-            _service = service;
+            _orderAppService = orderAppService;
+            _basketItemAppService = basketItemAppService;
         }
 
         public virtual async Task OnGetAsync()
@@ -44,14 +52,38 @@ namespace EasyAbp.EShop.Plugins.Baskets.Web.Pages.EShop.Plugins.Baskets.BasketIt
                 return;
             }
             
-            var dto = await _service.GetAsync(Id);
+            var dto = await _basketItemAppService.GetAsync(Id);
             ViewModel = ObjectMapper.Map<BasketItemDto, EditBasketItemViewModel>(dto);
         }
 
         public virtual async Task<IActionResult> OnPostAsync()
         {
+            var item = await _basketItemAppService.GetAsync(Id);
+
             var dto = ObjectMapper.Map<EditBasketItemViewModel, UpdateBasketItemDto>(ViewModel);
-            await _service.UpdateAsync(Id, dto);
+            
+            var checkCreateOrderResult = await _orderAppService.CheckCreateAsync(new CheckCreateOrderInput
+            {
+                StoreId = item.StoreId,
+                OrderLines = new List<CreateOrderLineDto>
+                {
+                    new()
+                    {
+                        ProductId = item.ProductId,
+                        ProductSkuId = item.ProductSkuId,
+                        Quantity = ViewModel.Quantity
+                    }
+                }
+            });
+
+            if (!checkCreateOrderResult.CanCreate)
+            {
+                throw new BusinessException(BasketsErrorCodes.CheckCreateOrderFailed)
+                    .WithData("reason", checkCreateOrderResult.Reason);
+            }
+            
+            await _basketItemAppService.UpdateAsync(Id, dto);
+            
             return NoContent();
         }
     }
