4ed29f33549328d576bad79c68e6221ed32f0fd0
gdlcf88 <gdlcf88@outlook.com>
2023-03-16 00:09:47 +0800

    Register `EShopPaymentsObjectExtensions`

---
+++ modules/EasyAbp.EShop.Payments/src/EasyAbp.EShop.Payments.Domain/EasyAbp/EShop/Payments/ObjectExtending/EShopPaymentsObjectExtensions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using EasyAbp.EShop.Payments.Payments;
+using EasyAbp.EShop.Payments.Refunds;
+using EasyAbp.PaymentService.Payments;
+using EasyAbp.PaymentService.Refunds;
+using Volo.Abp.ObjectExtending;
+using Volo.Abp.Threading;
+
+namespace EasyAbp.EShop.Payments.ObjectExtending
+{
+    public static class EShopPaymentsObjectExtensions
+    {
+        private static readonly OneTimeRunner OneTimeRunner = new OneTimeRunner();
+
+        public static void Configure()
+        {
+            OneTimeRunner.Run(() =>
+            {
+                /* You can configure extension properties to entities or other object types
+                 * defined in the depended modules.
+                 * 
+                 * If you are using EF Core and want to map the entity extension properties to new
+                 * table fields in the database, then configure them in the DenturePlusEfCoreEntityExtensionMappings
+                 *
+                 * Example:
+                 *
+                 * ObjectExtensionManager.Instance
+                 *    .AddOrUpdateProperty<IdentityRole, string>("Title");
+                 *
+                 * See the documentation for more:
+                 * https://docs.abp.io/en/abp/latest/Object-Extensions
+                 */
+
+                ObjectExtensionManager.Instance
+                    .AddOrUpdate(new []
+                    {
+                        typeof(PaymentItem),
+                        typeof(EShopPaymentItemEto),
+                        typeof(PaymentItemEto),
+                        typeof(CreatePaymentItemEto)
+                    }, options =>
+                    {
+                        options.AddOrUpdateProperty<Guid>(nameof(PaymentItem.StoreId));
+                    });
+
+                ObjectExtensionManager.Instance
+                    .AddOrUpdate(new []
+                    {
+                        typeof(RefundItem),
+                        typeof(EShopRefundItemEto),
+                        typeof(RefundItemEto),
+                        typeof(CreateRefundItemInput)
+                    }, options =>
+                    {
+                        options.AddOrUpdateProperty<Guid>(nameof(RefundItem.StoreId));
+                        options.AddOrUpdateProperty<Guid>(nameof(RefundItem.OrderId));
+                        options.AddOrUpdateProperty<string>(nameof(RefundItem.OrderLines));
+                        options.AddOrUpdateProperty<string>(nameof(RefundItem.OrderExtraFees));
+                    });
+            });
+        }
+    }
+}
--- modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.Application.Tests/Payments/TestCreatePaymentEventHandler.cs
+++ modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.Application.Tests/Payments/TestCreatePaymentEventHandler.cs
@@ -8,11 +8,14 @@ namespace EasyAbp.EShop.Payments.Payments
     public class TestCreatePaymentEventHandler : IDistributedEventHandler<CreatePaymentEto>, ISingletonDependency
     {
         public bool IsEventPublished { get; protected set; }
-        
+
+        public CreatePaymentEto CreatePaymentEto { get; protected set; }
+
         public Task HandleEventAsync(CreatePaymentEto eventData)
         {
             IsEventPublished = true;
-            
+            CreatePaymentEto = eventData;
+
             return Task.CompletedTask;
         }
     }
