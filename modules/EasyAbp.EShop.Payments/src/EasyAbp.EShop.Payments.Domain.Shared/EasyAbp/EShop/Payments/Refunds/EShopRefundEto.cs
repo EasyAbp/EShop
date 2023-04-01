717c7dacca8e543b98944513d1c5fda3561e9c3e
gdlcf88 <gdlcf88@outlook.com>
2023-04-01 15:30:40 +0800

    Upgrade to PaymentService v2.5.1

--- modules/EasyAbp.EShop.Payments/src/EasyAbp.EShop.Payments.Domain.Shared/EasyAbp/EShop/Payments/Refunds/EShopRefundEto.cs
+++ modules/EasyAbp.EShop.Payments/src/EasyAbp.EShop.Payments.Domain.Shared/EasyAbp/EShop/Payments/Refunds/EShopRefundEto.cs
@@ -16,27 +16,28 @@ namespace EasyAbp.EShop.Payments.Refunds
         public Guid? TenantId { get; set; }
 
         public Guid PaymentId { get; set; }
-        
+
         public string RefundPaymentMethod { get; set; }
-        
+
         public string ExternalTradingCode { get; set; }
-        
+
         public string Currency { get; set; }
-        
+
         public decimal RefundAmount { get; set; }
-        
+
         public string DisplayReason { get; set; }
 
         public string CustomerRemark { get; set; }
-        
+
         public string StaffRemark { get; set; }
-        
+
         public DateTime? CompletedTime { get; set; }
-        
+
         public DateTime? CanceledTime { get; set; }
-        
+
         #endregion
 
-        public List<EShopRefundItemEto> RefundItems { get; set; } = new List<EShopRefundItemEto>();
+        IEnumerable<IRefundItem> IRefund.RefundItems => RefundItems;
+        public List<EShopRefundItemEto> RefundItems { get; set; } = new();
     }
 }
