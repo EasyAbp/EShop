752940fb58a7ba7985221b57b18cbf83c745ff37
gdlcf88 <gdlcf88@outlook.com>
2024-07-30 23:22:29 +0800

    Upgrade to ABP 8.2.0

--- modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.EntityFrameworkCore.Tests/EntityFrameworkCore/EShopPaymentsEntityFrameworkCoreTestModule.cs
+++ modules/EasyAbp.EShop.Payments/test/EasyAbp.EShop.Payments.EntityFrameworkCore.Tests/EntityFrameworkCore/EShopPaymentsEntityFrameworkCoreTestModule.cs
@@ -32,7 +32,7 @@ namespace EasyAbp.EShop.Payments.EntityFrameworkCore
         
         private static SqliteConnection CreateDatabaseAndGetConnection()
         {
-            var connection = new SqliteConnection("Data Source=:memory:");
+            var connection = new AbpUnitTestSqliteConnection("Data Source=:memory:");
             connection.Open();
 
             new PaymentsDbContext(
