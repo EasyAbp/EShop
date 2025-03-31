4e9623236d8ff08faa6e535db84597eb15cf24dd
gdlcf88 <gdlcf88@outlook.com>
2025-03-31 17:52:08 +0800

    Upgrade to ABP 9.1.1

--- samples/EShopSample/aspnet-core/src/EShopSample.EntityFrameworkCore/Migrations/20250331095106_Upgraded_To_Abp_9_1.Designer.cs
+++ samples/EShopSample/aspnet-core/src/EShopSample.EntityFrameworkCore/Migrations/20250331095106_Upgraded_To_Abp_9_1.Designer.cs
@@ -4,6 +4,7 @@ using EShopSample.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Volo.Abp.EntityFrameworkCore;
 
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace EShopSample.Migrations
 {
     [DbContext(typeof(EShopSampleDbContext))]
-    partial class EShopSampleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250331095106_Upgraded_To_Abp_9_1")]
+    partial class Upgraded_To_Abp_9_1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
